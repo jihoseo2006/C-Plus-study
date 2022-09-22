14b0114762d118d78060adbec847b97632c44ae7
jihoseo2006 <jihoseo2006@gmail.com>
2022-09-22 10:09:29 +0900

    일차원 배열식을 이차원 배열로 출력하기
    성공!

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,35 +16,87 @@
 //	}
 //}
 
-#include <iostream>
+//#include <iostream>
+//using namespace std;
+//
+//class mytime {
+//private:
+//	int hh, mm, ss;
+//	int tss;
+//public:
+//	mytime();
+//	mytime(int h, int m, int s);
+//	void showme();
+//	mytime cvt_s2h(int n);
+//	mytime operator + (mytime t);
+//	mytime operator - (mytime t);
+//	mytime operator * (int n);
+//	friend mytime operator * (int n, mytime t);
+//	mytime operator / (int n);
+//};
+//
+//int main() {
+//	mytime result;
+//	mytime t1(7, 29, 48); t1.showme();
+//	mytime t2(4, 54, 39); t2.showme();
+//
+//	result = t1 + t1 + t1; result.showme();
+//	result = t2 - t1 + t1; result.showme();
+//	result = t1 * 3; result.showme();
+//	result = 3 * t1; result.showme();
+//	result = t2 / 4; result.showme();
+//	result = result.cvt_s2h(300); result.showme();
+//}
+//mytime::mytime() {
+//	hh = mm = ss = tss = 0;
+//}
+//
+//mytime::mytime(int h, int m,int s) {
+//	hh = h; mm = m; ss = s;
+//	tss = h * 60 * 60 + m * 60 + s;
+//}
+//void mytime::showme() {
+//	cout.width(3);   cout << hh << " :";
+//	cout.width(3);   cout << mm <<  " :";
+//	cout.width(3);   cout << ss << "\t";
+//	cout.width(3);   cout << tss << endl;
+//}
+//mytime mytime::cvt_s2h(int tt) {
+//	tt = abs(tt);
+//	mytime result(tt / 60 / 60, tt / 60 % 60, tt % 60);
+//	return result;
+//}
+//mytime mytime::operator + (mytime t) {
+//	return cvt_s2h(tss + t.tss);
+//}
+//mytime mytime::operator - (mytime t) {
+//	return cvt_s2h(tss - t.tss);
+//}
+//mytime mytime::operator * (int n) {
+//	return cvt_s2h(tss * n);
+//}
+//mytime operator * (int n,mytime t) {
+//	return t * n;
+//}
+//mytime mytime::operator / (int n) {
+//	return cvt_s2h(tss / n);
+//}
+
+#include<iostream>//일차원 배열을 이차원 배열식으로 출력하기
+
 using namespace std;
 
-class MyTime {
-private:
-	int hh, mm, ss, tss;
-public:
-	int Cvt_H2S(MyTime tt);
-	MyTime Cvt_S2H(int ssp);
-	void showme(MyTime tt);
-	MyTime(int h, int m, int s);
-};
-void MyTime::showme(MyTime tt) {
-	cout << tt.hh << "시간\t" << tt.mm<< "분\t" << tt.ss << "초\t" << tt.tss << endl;
-}
-
-MyTime::MyTime(int h, int m, int s) {
-	hh = h, mm = m, ss = s;
-	tss = h * 60 * 60 + m * 60 + s;
-}
-
-int MyTime::Cvt_H2S(MyTime tt) {
-	tt.tss = tt.hh * 60 * 60 + tt.mm * 60 + tt.ss;
-	return tt.tss;
-}
-
 int main() {
-	MyTime t1(7,29,48);
+	int ary[] = { 6,2,9,3,8,4,5,1 };
+	int(*ary2)[2] = (int(*)[2])ary;
 
-	t1.showme(t1);
 
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 2; j++) {
+			cout << ary2[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
